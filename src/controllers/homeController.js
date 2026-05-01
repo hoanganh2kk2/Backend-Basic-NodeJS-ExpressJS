5d72736de8fb259ea0acfbe115ad4c4c114ac324
hoanganh2kk2 <hoanganh11042kk2@gmail.com>
2026-05-01 16:09:45 +0700

    Design NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,16 +1,7 @@
 const connection = require("../config/database");
 
 const getHomepage = (req, res) => {
-  //process data
-  //call model
-  let users = [];
-  connection.query("SELECT * FROM Users u", function (err, results, fields) {
-    users = results;
-    console.log(">>> results = ", results); // results contains rows returned by server
-
-    console.log(">>> check users: ", users);
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home");
 };
 
 const checkHelloWorld = (req, res) => {
